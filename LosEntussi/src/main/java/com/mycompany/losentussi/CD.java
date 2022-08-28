8651254b6e60961bcf4e3124982e67bf3b1561d7
Lazaro <larojas21@alumnos.utalca.cl>
2022-08-28 13:37:09 -0400

    Creacion de letras v,w,x,y,x

--- LosEntussi/src/main/java/com/mycompany/losentussi/CD.java
+++ LosEntussi/src/main/java/com/mycompany/losentussi/CD.java
@@ -214,6 +214,79 @@ public class CD extends javax.swing.JFrame{
         u.moveTo(130,55);
         u.curveTo(130,55,130,143,160,143);
         g2.draw(u);*/
+        
+        //v
+        /*
+        Path2D.Double v =new Path2D.Double();
+        v.moveTo(0,143);
+        v.curveTo(20,143,15,55,40,55);
+        v.moveTo(40,55);
+        v.curveTo(65,55,60,143,80,143);
+        v.moveTo(80,143);
+        v.curveTo(100,143,110,55,110,55);
+        v.moveTo(110,55);
+        v.curveTo(115,60,155,60,160,55);
+        g2.draw(v);*/
+        
+        //w
+        /*
+        Path2D.Double w =new Path2D.Double();
+        w.moveTo(0,143);
+        w.curveTo(20,143,15,55,40,55);
+        w.moveTo(40,55);
+        w.curveTo(65,55,60,143,80,143);
+        w.moveTo(80,143);
+        w.curveTo(120,153,110,55,110,55);
+        w.moveTo(110,55);
+        w.curveTo(110,55,100,153,140,143);       
+        w.moveTo(140,143);
+        w.curveTo(160,143,160,55,160,55);
+        w.moveTo(160,55);
+        w.curveTo(165,60,195,60,200,55);
+        g2.draw(w);*/
+        
+        //x
+        /*
+        Path2D.Double x =new Path2D.Double();
+        x.moveTo(0,143);
+        x.curveTo(60,143,60,55,100,55);
+        x.moveTo(0,55);
+        x.curveTo(50,55,50,143,100,143);
+        g2.draw(x);*/
+        
+        //y
+        /*
+        Path2D.Double  y =new Path2D.Double();
+        y.moveTo(0,143);
+        y.curveTo(0,143,30,143,30,55);
+        y.moveTo(30,55);
+        y.curveTo(20,174,80,138,80,138);
+        y.moveTo(80,55);
+        y.curveTo(80,55,80,200,80,200);
+        y.moveTo(80,200);
+        y.curveTo(80,225,40,225,40,200);
+        y.moveTo(40,200);
+        y.curveTo(40,180,60,143,120,143);
+        g2.draw(y);*/
+        
+        //z
+        /*
+        Path2D.Double  z =new Path2D.Double();
+        z.moveTo(20,143);
+        z.curveTo(50,143,0,90,70,90);
+        z.moveTo(70,90);
+        z.curveTo(100,90,90,143,70,143);
+        z.moveTo(70,143);
+        z.curveTo(45,143,45,125,70,130);     
+        z.moveTo(70,130);
+        z.curveTo(100,140,110,175,100,200);
+        z.moveTo(100,200);
+        z.curveTo(95,220,60,220,60,200);
+        z.moveTo(60,200);
+        z.curveTo(60,185,100,150,120,143);
+        g2.draw(z);*/
+        
+        
     }
 }
 
