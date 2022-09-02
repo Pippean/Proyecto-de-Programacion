16b9bc71e25bc74432cff8fff86b312663b38a76
Felipe Carvajal <fecarvajal21@alumnos.utalca.cl>
2022-09-02 11:25:13 -0400

    Prueba de paso de datos...

--- LosEntussi/src/main/java/com/mycompany/losentussi/Dibujar.java
+++ LosEntussi/src/main/java/com/mycompany/losentussi/Dibujar.java
@@ -10,10 +10,11 @@ import javax.swing.*;
 public class Dibujar{   
     
     public static String txtadibujar;
-    
+    static int w = 850;
+    static int h = 550;
+    static DibujarCanvas dc;
     public static void main(String[] args){
-        int w = 850;
-        int h = 550;
+        
         Button Convertir;
         Button Borrar;
         
@@ -24,17 +25,14 @@ public class Dibujar{
         TextField t1 = new TextField("  ");
         t1.setBounds(10, 480, 600, 22);  
         JFrame f = new JFrame();
-        DibujarCanvas dc = new DibujarCanvas(w,h);
-        
         f.setSize(w,h);
         f.setTitle("Conversor de Scripts");
         f.add(Convertir);
         f.add(Borrar);
         Borrar.setEnabled(false);
         f.add(t1);
-        f.add(dc);
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        f.setVisible(true);
+        cargarDC(f);
+       
         
         ActionListener accionConvertir= new ActionListener(){
             @Override
@@ -42,10 +40,14 @@ public class Dibujar{
                 txtadibujar = t1.getText();
                 System.out.println(""+ txtadibujar);
                 Convertir.setEnabled(false);
-                f.add(dc);
                 Borrar.setEnabled(true);
+                cargarDC(f);
             }
         };
+        f.add(dc);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setVisible(true);
+        
         Convertir.addActionListener(accionConvertir);
         
         ActionListener accionBorrar= new ActionListener(){
@@ -62,6 +64,9 @@ public class Dibujar{
         
     }
     
-    
+    public static void cargarDC(Frame f){
+        dc = new DibujarCanvas(w,h,"hola");
+        f.add(dc);
+    }
     
 }
--- LosEntussi/src/main/java/com/mycompany/losentussi/DibujarCanvas.java
+++ LosEntussi/src/main/java/com/mycompany/losentussi/DibujarCanvas.java
@@ -14,13 +14,20 @@ public class DibujarCanvas extends JComponent {
     private int width;
     private int height;
     private Minusculas m1;
+    private String texto;
     
     public DibujarCanvas(int w, int h){
         width = w;
         height = h;
         m1 = new Minusculas();
     }
-    
+    public DibujarCanvas(int w, int h, String texto){
+        width = w;
+        height = h;
+        m1 = new Minusculas();
+        this.texto = texto;
+    }
+  
     public void paintComponent(Graphics g){
         Graphics2D g2 = (Graphics2D)g;
         
@@ -28,8 +35,11 @@ public class DibujarCanvas extends JComponent {
             RenderingHints.KEY_ANTIALIASING,
             RenderingHints.VALUE_ANTIALIAS_ON);
         g2.setRenderingHints(rh);
-        
-        m1.dibujara(g2,250,0);
+        if(!texto.equals("hola"))
+            m1.dibujara(g2,250,0);
+        else{
+            m1.dibujarb(g2, 250, 0);
+        }
         //m1.dibujarb(g2,0,143);
         
     }
