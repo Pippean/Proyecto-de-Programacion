844dbbd21fc055f73eed4d74945d81c549d8dd04
Felipe Carvajal <fecarvajal21@alumnos.utalca.cl>
2022-09-04 02:24:52 -0300

    Listo el paso de Datos, Dibujo de letras, Switch, Borrar

--- LosEntussi/src/main/java/com/mycompany/losentussi/Dibujar.java
+++ LosEntussi/src/main/java/com/mycompany/losentussi/Dibujar.java
@@ -5,11 +5,11 @@ import javax.swing.*;
 
 /**
  *
- * @author Pipe, Juano, Lazaro
+ * @author Pipe, Juano, Lázaro
  */
 public class Dibujar{   
     
-    public static String txtadibujar;
+    public static String txtadibujar = "";
     static int w = 850;
     static int h = 550;
     static DibujarCanvas dc;
@@ -23,53 +23,49 @@ public class Dibujar{
         Convertir.setBounds(720, 480, 100, 22);
         Borrar = new Button("Borrar");
         Borrar.setBounds(615, 480, 100, 22);
-        TextField t1 = new TextField("  ");
+        TextField t1 = new TextField("");
         t1.setBounds(10, 480, 600, 22);  
         JFrame f = new JFrame();
         
         f.setSize(w,h);
         f.setTitle("Conversor de Scripts");
+        f.setResizable(false);
         f.add(Convertir);
         f.add(Borrar);
         Borrar.setEnabled(false);
         f.add(t1);
-        cargarDC(f);
+        cargarDC(f,txtadibujar);
        
-        
         ActionListener accionConvertir= new ActionListener(){
             @Override
             public void actionPerformed(ActionEvent e){
                 txtadibujar = t1.getText();
-                //dc.setTexto(txtadibujar);
-                System.out.println(""+ txtadibujar);
                 Convertir.setEnabled(false);
                 Borrar.setEnabled(true);
-                cargarDC(f);
+                cargarDC(f, txtadibujar);
             }
         };
-        f.add(dc);
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        f.setVisible(true);
-        
+                       
         Convertir.addActionListener(accionConvertir);
         
         ActionListener accionBorrar= new ActionListener(){
             @Override
             public void actionPerformed(ActionEvent e){
                 txtadibujar = "";
-                System.out.println(txtadibujar);
                 t1.setText("");
+                dc.setVisible(false);
                 Convertir.setEnabled(true);
                 Borrar.setEnabled(false);
             }
         };
         Borrar.addActionListener(accionBorrar);
         
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
     
-    public static void cargarDC(Frame f){
+    public static void cargarDC(Frame f, String txtadibujar){
         dc = new DibujarCanvas(w,h,txtadibujar);
         f.add(dc);
-    }
-    
+        f.setVisible(true);
+    }    
 }
--- LosEntussi/src/main/java/com/mycompany/losentussi/DibujarCanvas.java
+++ LosEntussi/src/main/java/com/mycompany/losentussi/DibujarCanvas.java
@@ -16,11 +16,6 @@ public class DibujarCanvas extends JComponent {
     private Minusculas m1;
     private String texto;
     
-    public DibujarCanvas(int w, int h){
-        width = w;
-        height = h;
-        m1 = new Minusculas();
-    }
     public DibujarCanvas(int w, int h, String texto){
         width = w;
         height = h;
@@ -35,33 +30,124 @@ public class DibujarCanvas extends JComponent {
             RenderingHints.KEY_ANTIALIASING,
             RenderingHints.VALUE_ANTIALIAS_ON);
         g2.setRenderingHints(rh);
-        
-        /*
+        int x= 0, y=0;
         if(!"".equals(texto)){
             for (int i = 0; i < texto.length(); i++) {
-                if(texto.charAt(i)== 'a'){
-                    m1.dibujara(g2,250,0);
-                }
-                if(texto.charAt(i)=='b'){
-                    m1.dibujarb(g2, 250, 0);
-                }
+                char caracter = texto.charAt(i);
+                switch(caracter){
+                    case ' ':{
+                        x+=160;
+                        break;
+                    }
+                    case 'a':{
+                        m1.dibujara(g2,x,y);
+                        break;
+                    }
+                    case 'b':{
+                        m1.dibujarb(g2, x, y);
+                        break;
+                    }
+                    case 'c':{
+                        m1.dibujarc(g2, x, y);
+                        break;
+                    }
+                    case 'd':{
+                        m1.dibujard(g2, x, y);
+                        break;
+                    }
+                    case 'e':{
+                        m1.dibujarb(g2, x, y);
+                        break;
+                    }
+                    case 'f':{
+                        m1.dibujarb(g2, x, y);
+                        break;
+                    }
+                    case 'g':{
+                        m1.dibujarb(g2, x, y);
+                        break;
+                    }
+                    case 'h':{
+                        m1.dibujarb(g2, x, y);
+                        break;
+                    }
+                    case 'i':{
+                        m1.dibujarb(g2, x, y);
+                        break;
+                    }
+                    case 'j':{
+                        m1.dibujarb(g2, x, y);
+                        break;
+                    }
+                    case 'k':{
+                        m1.dibujarb(g2, x, y);
+                        break;
+                    }
+                    case 'm':{
+                        m1.dibujarb(g2, x, y);
+                        break;
+                    }
+                    case 'n':{
+                        m1.dibujarb(g2, x, y);
+                        break;
+                    }
+                    case 'ñ':{
+                        m1.dibujarb(g2, x, y);
+                        break;
+                    }
+                    case 'o':{
+                        m1.dibujarb(g2, x, y);
+                        break;
+                    }
+                    case 'p':{
+                        m1.dibujarb(g2, x, y);
+                        break;
+                    }
+                    case 'q':{
+                        m1.dibujarb(g2, x, y);
+                        break;
+                    }
+                    case 'r':{
+                        m1.dibujarb(g2, x, y);
+                        break;
+                    }
+                    case 's':{
+                        m1.dibujarb(g2, x, y);
+                        break;
+                    }
+                    case 't':{
+                        m1.dibujarb(g2, x, y);
+                        break;
+                    }
+                    case 'u':{
+                        m1.dibujarb(g2, x, y);
+                        break;
+                    }
+                    case 'v':{
+                        m1.dibujarb(g2, x, y);
+                        break;
+                    }
+                    case 'w':{
+                        m1.dibujarb(g2, x, y);
+                        break;
+                    }
+                    case 'x':{
+                        m1.dibujarb(g2, x, y);
+                        break;
+                    }
+                    case 'y':{
+                        m1.dibujarb(g2, x, y);
+                        break;
+                    }
+                    case 'z':{
+                        m1.dibujarb(g2, x, y);
+                        break;
+                    }
+                    default:{
+                        break;
+                    } 
+                }    
             }
-        }
-        */
-        
-        if(texto == null){
-            System.out.println("No hay texto ingresado");
-        }
-        else{
-            System.out.println(texto);
-           if(!texto.equals("hola"))
-            m1.dibujara(g2,250,0);
-        else{
-            m1.dibujarb(g2, 250, 0);
-        } 
-        }
-        
-        //m1.dibujarb(g2,0,143);
-           
+        }    
     }
 }
