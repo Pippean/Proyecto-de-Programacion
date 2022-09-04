fe59d8466f988a60af0a91f828338a9c06e27e65
Felipe Carvajal <fecarvajal21@alumnos.utalca.cl>
2022-09-04 02:32:25 -0300

    Actualización Clase DibujarCanvas

--- LosEntussi/src/main/java/com/mycompany/losentussi/DibujarCanvas.java
+++ LosEntussi/src/main/java/com/mycompany/losentussi/DibujarCanvas.java
@@ -14,13 +14,15 @@ public class DibujarCanvas extends JComponent {
     private int width;
     private int height;
     private Minusculas m1;
+    private String texto;
     
-    public DibujarCanvas(int w, int h){
+    public DibujarCanvas(int w, int h, String texto){
         width = w;
         height = h;
         m1 = new Minusculas();
+        this.texto = texto;
     }
-    
+  
     public void paintComponent(Graphics g){
         Graphics2D g2 = (Graphics2D)g;
         
@@ -28,9 +30,124 @@ public class DibujarCanvas extends JComponent {
             RenderingHints.KEY_ANTIALIASING,
             RenderingHints.VALUE_ANTIALIAS_ON);
         g2.setRenderingHints(rh);
-        
-        m1.dibujara(g2,250,0);
-        //m1.dibujarb(g2,0,143);
-        
+        int x= 0, y=0;
+        if(!"".equals(texto)){
+            for (int i = 0; i < texto.length(); i++) {
+                char caracter = texto.charAt(i);
+                switch(caracter){
+                    case ' ':{
+                        x+=160;
+                        break;
+                    }
+                    case 'a':{
+                        m1.dibujara(g2,x,y);
+                        break;
+                    }
+                    case 'b':{
+                        m1.dibujarb(g2, x, y);
+                        break;
+                    }
+                    case 'c':{
+                        m1.dibujarc(g2, x, y);
+                        break;
+                    }
+                    case 'd':{
+                        m1.dibujard(g2, x, y);
+                        break;
+                    }
+                    case 'e':{
+                        m1.dibujarb(g2, x, y);
+                        break;
+                    }
+                    case 'f':{
+                        m1.dibujarb(g2, x, y);
+                        break;
+                    }
+                    case 'g':{
+                        m1.dibujarb(g2, x, y);
+                        break;
+                    }
+                    case 'h':{
+                        m1.dibujarb(g2, x, y);
+                        break;
+                    }
+                    case 'i':{
+                        m1.dibujarb(g2, x, y);
+                        break;
+                    }
+                    case 'j':{
+                        m1.dibujarb(g2, x, y);
+                        break;
+                    }
+                    case 'k':{
+                        m1.dibujarb(g2, x, y);
+                        break;
+                    }
+                    case 'm':{
+                        m1.dibujarb(g2, x, y);
+                        break;
+                    }
+                    case 'n':{
+                        m1.dibujarb(g2, x, y);
+                        break;
+                    }
+                    case 'ñ':{
+                        m1.dibujarb(g2, x, y);
+                        break;
+                    }
+                    case 'o':{
+                        m1.dibujarb(g2, x, y);
+                        break;
+                    }
+                    case 'p':{
+                        m1.dibujarb(g2, x, y);
+                        break;
+                    }
+                    case 'q':{
+                        m1.dibujarb(g2, x, y);
+                        break;
+                    }
+                    case 'r':{
+                        m1.dibujarb(g2, x, y);
+                        break;
+                    }
+                    case 's':{
+                        m1.dibujarb(g2, x, y);
+                        break;
+                    }
+                    case 't':{
+                        m1.dibujarb(g2, x, y);
+                        break;
+                    }
+                    case 'u':{
+                        m1.dibujarb(g2, x, y);
+                        break;
+                    }
+                    case 'v':{
+                        m1.dibujarb(g2, x, y);
+                        break;
+                    }
+                    case 'w':{
+                        m1.dibujarb(g2, x, y);
+                        break;
+                    }
+                    case 'x':{
+                        m1.dibujarb(g2, x, y);
+                        break;
+                    }
+                    case 'y':{
+                        m1.dibujarb(g2, x, y);
+                        break;
+                    }
+                    case 'z':{
+                        m1.dibujarb(g2, x, y);
+                        break;
+                    }
+                    default:{
+                        break;
+                    } 
+                }    
+            }
+        }    
     }
 }
