1353f1094cb985255e8b7c3ce7aa77e43f0659fc
Felipe Carvajal <fecarvajal21@alumnos.utalca.cl>
2022-09-05 01:14:38 -0300

    Arreglo de Branch Clase Dibujar

--- LosEntussi/src/main/java/com/mycompany/losentussi/Dibujar.java
+++ LosEntussi/src/main/java/com/mycompany/losentussi/Dibujar.java
@@ -5,63 +5,84 @@ import javax.swing.*;
 
 /**
  *
- * @author Pipe, Juano, Lazaro
+ * @author Pipe, Juano, Lázaro
  */
 public class Dibujar{   
     
-    public static String txtadibujar;
+    public static String txtadibujar = "";
+    public static String color = "";
+    static int w = 1300;
+    static int h = 1080;
+    static DibujarCanvas dc;
+    
     
     public static void main(String[] args){
-        int w = 850;
-        int h = 550;
+        
         Button Convertir;
         Button Borrar;
+        Choice menu;
         
         Convertir = new Button("Convertir");
-        Convertir.setBounds(720, 480, 100, 22);
+        Convertir.setBounds(1050, 700, 100, 22);
         Borrar = new Button("Borrar");
-        Borrar.setBounds(615, 480, 100, 22);
-        TextField t1 = new TextField("  ");
-        t1.setBounds(10, 480, 600, 22);  
+        Borrar.setBounds(1170, 700, 100, 22);
+        menu = new Choice();
+        menu.setBounds(1170, 10, 100, 20);
+        menu.add("Negro");
+        menu.add("Rojo");
+        menu.add("Azul");
+        menu.add("Amarillo");
+        menu.add("Verde");
+        menu.add("Naranjo");
+        menu.add("Morado");
+        menu.add("Rosado");
+        menu.add("Celeste");
+        menu.add("Blanco");
+        TextField t1 = new TextField("");
+        t1.setBounds(10, 700, 1000, 22);  
         JFrame f = new JFrame();
-        DibujarCanvas dc = new DibujarCanvas(w,h);
         
         f.setSize(w,h);
         f.setTitle("Conversor de Scripts");
+        f.setResizable(false);
         f.add(Convertir);
         f.add(Borrar);
+        f.add(menu);
         Borrar.setEnabled(false);
         f.add(t1);
-        f.add(dc);
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        f.setVisible(true);
-        
+        cargarDC(f,txtadibujar, color);
+       
         ActionListener accionConvertir= new ActionListener(){
             @Override
             public void actionPerformed(ActionEvent e){
                 txtadibujar = t1.getText();
-                System.out.println(""+ txtadibujar);
                 Convertir.setEnabled(false);
-                f.add(dc);
-                Borrar.setEnabled(true);
+                Borrar.setEnabled(true);   
+                color = menu.getItem(menu.getSelectedIndex());
+                cargarDC(f, txtadibujar, color);
             }
         };
+                       
         Convertir.addActionListener(accionConvertir);
         
         ActionListener accionBorrar= new ActionListener(){
             @Override
             public void actionPerformed(ActionEvent e){
-                txtadibujar = " ";
-                System.out.println(txtadibujar);
+                txtadibujar = "";
                 t1.setText("");
+                dc.setVisible(false);
                 Convertir.setEnabled(true);
                 Borrar.setEnabled(false);
             }
         };
         Borrar.addActionListener(accionBorrar);
         
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
     
-    
-    
+    public static void cargarDC(Frame f, String txtadibujar, String color){
+        dc = new DibujarCanvas(w,h,txtadibujar, color);
+        f.add(dc);
+        f.setVisible(true);
+    }    
 }
